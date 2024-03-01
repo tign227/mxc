a74ea357b6bdc2e3d6529e033ce5d8a661826f92
TingHuang <ting@astriddao.xyz>
2024-03-01 13:01:54 +0800

    deploy and veridy task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+// require("@nomicfoundation/hardhat-verify");
+require("./task/DeployAndVerify");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.0",
   networks: {
     sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.SEPOLIA_RPC_URL}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_SEPOLIA_API}`,
+      accounts: [process.env.PRIVATE_KEY]
     }
+  },
+  etherscan:{
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    enabled: true
   }
+  // npx hardhat verify --network sepolia 0x7098482880DD70cA9eBd6fa1c3D49cc8740e5F4A
+
 };
---
+++ task/DeployAndVerify.js
@@ -0,0 +1,19 @@
+
+const { task } = require("hardhat/config");
+
+task("deployAndVerify", "Deploys and verifies the ETHPool contract")
+  .setAction(async (taskArgs, { ethers, run }) => {
+    const [deployer] = await ethers.getSigners();
+
+    console.log("Deploying ETHPool contract with the account:", deployer.address);
+
+    const ETHPool = await ethers.getContractFactory("ETHPool");
+    const ethPool = await ETHPool.deploy();
+    console.log("ETHPool deployed to:", ethPool.target);
+
+    // Verify the contract
+    await run("verify:verify", {
+      address: ethPool.target,
+      constructorArguments: [],
+    });
+  });
